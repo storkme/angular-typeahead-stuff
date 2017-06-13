3a77d28cf22b0ffe851a4b6381778136f127c98d
alex.storkey <alex.storkey@eckoh.com>
2017-06-13 08:26:19 +0100

    updating html codez

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { TypeaheadComponent } from './components/typeahead.component';
+import { AsyncErrorPipe } from './components/async-error.pipe';
+import { TestAsyncErrorComponent } from './components/test-async-error.component';
 import { TypeaheadInputDirective } from './components/typeahead-input.directive';
 import { TypeaheadOptionComponent } from './components/typeahead-option.component';
-import { HttpModule } from '@angular/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TestAsyncErrorComponent } from './components/test-async-error.component';
+import { TypeaheadComponent } from './components/typeahead.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { TestAsyncErrorComponent } from './components/test-async-error.component
     TypeaheadComponent,
     TypeaheadInputDirective,
     TypeaheadOptionComponent,
-    TestAsyncErrorComponent
+    TestAsyncErrorComponent,
+    AsyncErrorPipe
   ],
   imports: [
     BrowserModule,
